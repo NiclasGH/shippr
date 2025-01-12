f901c62ddeeb9f6dd3115688fda27ceebff9ea7f
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-01-12 18:26:26 +0100

    create test for config and implement it using a generic directory and filename

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,62 @@
-use std::env;
+use std::path::PathBuf;
 
 use config::File;
+use serde::Deserialize;
 
-use crate::Result;
+use crate::{Error, Result};
 
-struct AppConfig;
-//impl AppConfig {
-//    pub fn new() -> Result<Self> {
-//        let path = env::var("CARGO_MANIFEST_DIR").unwrap_or(String::from("."));
-//
-//        // environment underscores are kept: AXUM_BASE_URL etc BUT, it cant work with _ in variable names.
-//        // Therefor we use __ to separate the normal nesting and variable names
-//        let config = config::Config::builder()
-//            .add_source(File::with_name(&format!(
-//                "deployment"
-//            )))
-//            .build()?
-//            .try_deserialize()?;
-//
-//        Ok(config)
-//    }
-//}
+#[derive(Debug, Deserialize)]
+struct AppConfig {
+    chart: String,
+}
+impl AppConfig {
+    pub fn new(base_path: &PathBuf, file_name: &str) -> Result<Self> {
+        let directory_string = base_path.as_os_str().to_str().ok_or(
+            Error::InvalidDirectory
+        )?;
+        let config_path = format!("{directory_string}/{file_name}");
 
+        let config = config::Config::builder()
+            .add_source(File::with_name(&config_path))
+            .build()?
+            .try_deserialize()?;
+
+        Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+    use tempfile::Builder;
+
+    use super::AppConfig;
+    type TestResult = std::result::Result<(), Box<dyn std::error::Error>>;
+
+    #[test]
+    fn file_deserialized_correctly() -> TestResult {
+        // given
+        let mut deployment_file = Builder::new()
+            .prefix("deployment")
+            .suffix(".yaml")
+            .tempfile()?;
+        let file_content = r#"
+        chart: MyChart
+        "#;
+        writeln!(&mut deployment_file, "{}", file_content)?;
+        let binding = deployment_file.into_temp_path();
+        let file_name = binding
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
+
+        // when
+        let result = AppConfig::new(&std::env::temp_dir(), file_name)?;
+
+        // then
+        assert_eq!(result.chart, "MyChart");
+
+        Ok(())
+    }
+}
--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,19 @@
+use std::fmt::Display;
+
+use derive_more::derive::From;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
+#[derive(Debug, From)]
 pub enum Error {
+    InvalidDirectory,
+    ConfigError(#[from] config::ConfigError),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+impl std::error::Error for Error {
 }
