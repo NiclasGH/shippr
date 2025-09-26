b67db0644e6975bf1a21aeba36fbdd60c4037822
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-09-26 15:58:33 +0200

    add second print to ask for manual editing

--- src/actions/init.rs
+++ src/actions/init.rs
@@ -10,9 +10,14 @@ pub fn initialize_configuration(deployment_name: String) -> Result<()> {
     create_file(&deployment_name, "values-default.yaml", None)?;
 
     let default_deployment_file = create_default_deployment(&deployment_name);
-    create_file(&deployment_name, "deployment.yaml", Some(&default_deployment_file))?;
+    create_file(
+        &deployment_name,
+        "deployment.yaml",
+        Some(&default_deployment_file),
+    )?;
 
     println!("Created deployment files for {deployment_name}");
+    println!("Please configure the namespace, version, location and values before deploying");
 
     Ok(())
 }
@@ -28,7 +33,11 @@ fn create_dir(name: &str) -> Result<()> {
     Ok(())
 }
 
-fn create_file(directory_name: &str, file_name: &str, content_optional: Option<&str>) -> Result<()> {
+fn create_file(
+    directory_name: &str,
+    file_name: &str,
+    content_optional: Option<&str>,
+) -> Result<()> {
     if directory_name.contains("/") {
         return Err(Error::ReleaseNameIsPath);
     }
@@ -44,12 +53,12 @@ fn create_file(directory_name: &str, file_name: &str, content_optional: Option<&
 }
 
 fn create_default_deployment(deployment_name: &str) -> String {
-    format!(r#"name: {deployment_name}
+    format!(
+        r#"name: {deployment_name}
 version: 1.0
 # namespace: default
 location: # TODO replace with actual location
   # repo: https://artifacthub.io/{deployment_name}
-  # local: /home/root/charts/{deployment_name}"#)
+  # local: /home/root/charts/{deployment_name}"#
+    )
 }
-
-
