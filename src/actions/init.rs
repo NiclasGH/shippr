e41a0dd517408203db69020482deec511e63d4b5
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-09-26 14:45:19 +0200

    add first working solution for deployment initialization

--- src/actions/init.rs
+++ src/actions/init.rs
@@ -1,16 +1,18 @@
 use tracing::{debug, info};
 
 use crate::{Error, Result};
-use std::fs;
+use std::{fs, io::Write};
 
-pub fn initialize_configuration(name: String) -> Result<()> {
-    debug!("Received the following parameters: name: [{name:?}");
+pub fn initialize_configuration(deployment_name: String) -> Result<()> {
+    debug!("Received the following parameters: name: [{deployment_name:?}");
 
-    create_dir(&name)?;
+    create_dir(&deployment_name)?;
+    create_file(&deployment_name, "values-default.yaml", None)?;
 
-    todo!("Unimplemented");
-    // TODO create deployment file
-    // TODO create empty values-default.yaml
+    let default_deployment_file = create_default_deployment(&deployment_name);
+    create_file(&deployment_name, "deployment.yaml", Some(&default_deployment_file))?;
+
+    println!("Created deployment files for {deployment_name}");
 
     Ok(())
 }
@@ -21,7 +23,33 @@ fn create_dir(name: &str) -> Result<()> {
     }
 
     fs::create_dir(name)?;
-    info!("Created directory");
+    info!("Created directory: {name}");
 
     Ok(())
 }
+
+fn create_file(directory_name: &str, file_name: &str, content_optional: Option<&str>) -> Result<()> {
+    if directory_name.contains("/") {
+        return Err(Error::ReleaseNameIsPath);
+    }
+
+    let file_path = format!("{directory_name}/{file_name}");
+    let mut file = fs::File::create(&file_path)?;
+    if let Some(content) = content_optional {
+        file.write_all(content.as_bytes())?;
+    }
+    info!("Created file: {file_path}");
+
+    Ok(())
+}
+
+fn create_default_deployment(deployment_name: &str) -> String {
+    format!(r#"name: {deployment_name}
+version: 1.0
+# namespace: default
+location: # TODO replace with actual location
+  # repo: https://artifacthub.io/{deployment_name}
+  # local: /home/root/charts/{deployment_name}"#)
+}
+
+
